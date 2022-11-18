ff421345c7e497957019fe8d47bf9acc4f2af173
ken <1136728155@qq.com>
2022-11-18 15:26:08 +0800

    登录功能

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import axios from 'axios'
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
 import './plugins/element.js'
+import router from './router'
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8081/api/v1/'
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from './../components/UserLogin.vue'
+import Home from './../components/WebHome.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
+    component: Home
+  }
+]
 
 const router = new VueRouter({
   routes
+})
+
+// to：将要访问的路径
+// from: 从那个路径来的
+// next:是一个函数，表示放行，next()或next(‘/login’)
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
